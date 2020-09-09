e5dc1b3331444ffb9fca9e5156d298912d4f91eb
Antonio Celso <antonio.ti.celso@gmail.com>
2020-09-09 00:59:19 -0300

    test: Criacao de novos modelos de testes. 08/09/2020

--- src/test/java/br/com/tonhao/servicos/LocacaoServiceTest.java
+++ src/test/java/br/com/tonhao/servicos/LocacaoServiceTest.java
@@ -7,9 +7,7 @@ import br.com.tonhao.exception.FilmeSemEstoqueException;
 import br.com.tonhao.exception.LocadoraException;
 import br.com.tonhao.utils.DataUtils;
 import org.hamcrest.CoreMatchers;
-import org.junit.Assert;
-import org.junit.Rule;
-import org.junit.Test;
+import org.junit.*;
 import org.junit.rules.ErrorCollector;
 import org.junit.rules.ExpectedException;
 
@@ -17,22 +15,29 @@ import java.util.Date;
 
 public class LocacaoServiceTest {
 
+    private LocacaoService locacaoService;
+
     @Rule
     public ErrorCollector error = new ErrorCollector();
 
     @Rule
     public ExpectedException exception = ExpectedException.none();
 
+    @Before
+    public void setup() {
+        System.out.println("Before");
+        locacaoService = new LocacaoService();
+    }
+
     @Test
     public void deveFazerLocacao() throws Exception {
 
 //        cenario
-        LocacaoService service = new LocacaoService();
         Usuario usuario = new Usuario("Jorget");
         Filme filme = new Filme("Equilibriun", 2, 5.0);
 
 //        acao
-        Locacao locacao = service.alugarFilme(usuario, filme);
+        Locacao locacao = locacaoService.alugarFilme(usuario, filme);
 
 //        verificacao
         Assert.assertEquals(5.0, locacao.getValor(), 0.01);
@@ -54,12 +59,11 @@ public class LocacaoServiceTest {
     public void deveFazerLocacaoSemEstoque() throws Exception {
 
 //        cenario
-        LocacaoService service = new LocacaoService();
         Usuario usuario = new Usuario("Jorget");
         Filme filme = new Filme("Equilibriun", 0, 5.0);
 
 //        acao
-        service.alugarFilme(usuario, filme);
+        locacaoService.alugarFilme(usuario, filme);
 
     }
 
@@ -67,13 +71,12 @@ public class LocacaoServiceTest {
     public void deveFazerLocacaoSemEstoque_2() {
 
 //        cenario
-        LocacaoService service = new LocacaoService();
         Usuario usuario = new Usuario("Jorget");
         Filme filme = new Filme("Equilibriun", 0, 5.0);
 
 //        acao
         try {
-            service.alugarFilme(usuario, filme);
+            locacaoService.alugarFilme(usuario, filme);
             Assert.fail("Deveria ter lancado uma excecao!");
         } catch (Exception e) {
             Assert.assertThat(e.getMessage(), CoreMatchers.is("Filme sem estoque."));
@@ -86,7 +89,6 @@ public class LocacaoServiceTest {
     public void deveFazerLocacaoSemEstoque_3() throws Exception {
 
 //        cenario
-        LocacaoService service = new LocacaoService();
         Usuario usuario = new Usuario("Jorget");
         Filme filme = new Filme("Equilibriun", 0, 5.0);
 
@@ -94,19 +96,18 @@ public class LocacaoServiceTest {
         exception.expectMessage("Filme sem estoque.");
 
 //        acao
-        service.alugarFilme(usuario, filme);
+        locacaoService.alugarFilme(usuario, filme);
 
     }
 
     @Test
     public void deveFazerLocacaoSemUsuario() throws FilmeSemEstoqueException {
 //        cenario
-        LocacaoService service = new LocacaoService();
         Filme filme = new Filme("Equilibriun", 3, 5.0);
 
 //        acao
         try {
-            service.alugarFilme(null, filme);
+            locacaoService.alugarFilme(null, filme);
             Assert.fail("Deveria ter lancado uma excecao!");
         } catch (LocadoraException e) {
             Assert.assertThat(e.getMessage(), CoreMatchers.is("Usuario nao selecionado."));
@@ -119,14 +120,13 @@ public class LocacaoServiceTest {
     public void deveFazerLocacaoSemFilme() throws FilmeSemEstoqueException, LocadoraException {
 
 //        cenario
-        LocacaoService service = new LocacaoService();
         Usuario usuario = new Usuario("Jorget");
 
         exception.expect(LocadoraException.class);
         exception.expectMessage("Filme nao selecionado.");
 
 //        acao
-        service.alugarFilme(usuario, null);
+        locacaoService.alugarFilme(usuario, null);
 
     }
 
